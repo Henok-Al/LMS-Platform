203b4320b37d337b97fb9b041868bfaa0d43b79f
Henok-Al <henok.gebresenbet@gmail.com>
2025-04-17 21:24:47 +0300

    home page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.freepik.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/components/layout/main-layout.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "./navbar";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
